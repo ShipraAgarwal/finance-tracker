75ac165eb165624bbd1d6d1fff5391819f5968d1
ShipraAgarwal <shipraa137@gmail.com>
2018-08-29 16:53:55 +0530

    completed basic user portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   has_many :user_stocks
+   has_many :stocks, through: :user_stocks
+
+   def stock_already_exist?(ticker_symbol)
+   	  stock = Stock.find_by_ticker(ticker_symbol)
+   	  return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+   end
+
+   def max_limit?
+   	  (user_stocks.count < 10)
+   end
+
+   def can_add_stock?(ticker_symbol)
+   	  (max_limit? && !stock_already_exist?(ticker_symbol))
+   end
 end
